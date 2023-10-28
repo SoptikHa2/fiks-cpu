ba434284e6ce9f37c3632c35fb621ea41ca975ce
Petr Šťastný <petr.stastny@second-foundation.eu>
2023-10-28 20:41:27 +0200

    fix teleport, logging memory

--- instructions/TELEPORT.py
+++ instructions/TELEPORT.py
@@ -1,4 +1,6 @@
-from instructions.base import Instruction, ProgramError
+from instructions.base import Instruction
+
+_SPENT_CYCLES_WAITING: dict[str, int] = {}
 
 
 class TELEPORT(Instruction):
@@ -8,4 +10,23 @@ class TELEPORT(Instruction):
         timeout = self._parse_params(0, 8)
         jmp = self._parse_params(8, 16)
 
-        raise ValueError("Not implemented yet")
+        # If there is someone to teleport with
+        if len([x for x in self._state.player_teleport if x != self._player_id]) > 0:
+            to_teleport = self._state.player_teleport.pop()
+
+            self._state.player_pc[to_teleport] = self._pc + 1
+            # Return value so that we are moved to the other player's PC
+            return (self._state.player_pc[to_teleport] + 1) - self._pc
+        else:
+            self._state.player_teleport.add(self._player_id)
+
+            if self._player_id in _SPENT_CYCLES_WAITING:
+                _SPENT_CYCLES_WAITING[self._player_id] += 1
+                if _SPENT_CYCLES_WAITING[self._player_id] > timeout:
+                    _SPENT_CYCLES_WAITING[self._player_id] = 0
+                    self._state.player_teleport.remove(self._player_id)
+                    return jmp
+            else:
+                _SPENT_CYCLES_WAITING[self._player_id] = 0
+
+        return 0
--- log.py
+++ log.py
@@ -6,13 +6,16 @@ from process import Process
 @dataclass
 class TurnSnapshot:
     registers: list[int]
-    memory: list[int]
     next_instruction: int
+    memory: list[int]
+    memory_dump_start: int | None
+
 
 @dataclass
 class PlayerLog:
     turns: list[TurnSnapshot]
     death_reason: str | None = None
+
 
 @dataclass
 class Log:
@@ -23,19 +26,23 @@ class Log:
 
     def append_turn(self, process: Process):
         player_id = process.user_id
-        registers = process.registers
-        memory = process.state.memory[max(0,process.pc-10):min(len(process.state.memory)-1, process.pc+10)]
+        registers = process.registers.copy()
         instruction = process.state.memory[process.pc]
 
-        if not self.log_memory:
-            memory = []
+        memory = []
+        memory_dump_start = None
+        if self.log_memory:
+            memory = process.state.memory[max(0, process.pc - 10):min(len(process.state.memory) - 1, process.pc + 10)]
+            memory_dump_start = max(0, process.pc - 10)
 
         if player_id not in self.players:
             self.players[player_id] = PlayerLog([])
-        self.players[player_id].turns.append(TurnSnapshot(registers, memory, instruction))
+        self.players[player_id].turns.append(TurnSnapshot(registers, instruction, memory, memory_dump_start))
 
     def record_death(self, process: Process, reason: str):
         player_id = process.user_id
+        if player_id not in self.players:
+            self.players[player_id] = PlayerLog([])
         self.players[player_id].death_reason = reason
         self.survived[player_id] = len(self.players[player_id].turns)
 
